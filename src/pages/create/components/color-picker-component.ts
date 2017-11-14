3690b6a78a1b6c0d9626ab1d2a733e9bbfc58c6e
Quinn James <quinn@quinnjames.net>
2017-11-14 13:58:31 -0500

    Add an actual color picker

--- src/pages/create/components/color-picker-component.ts
+++ src/pages/create/components/color-picker-component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'color-picker',
   template: `
-  <div>Color picker</div>
+  <div>Color picker
+  <p>
+  <input type="color" id="html5colorpicker"
+  value="#ff0000">
+  </p></div>
   `
 })
 export class ColorPickerComponent{
