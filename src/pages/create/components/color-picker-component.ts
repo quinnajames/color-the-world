6abe3e7a0278a32208ca8fa0932be814c0f13ac4
Quinn James <quinn@quinnjames.net>
2017-11-15 13:19:09 -0500

    Preliminary way to console-log picked value

--- src/pages/create/components/color-picker-component.ts
+++ src/pages/create/components/color-picker-component.ts
@@ -5,12 +5,18 @@ import { Component } from '@angular/core';
   <div>Color picker
   <p>
   <input type="color" id="html5colorpicker"
-  value="#ff0000">
+  [(value)]="value" (change)="colorChange($event)">
   </p></div>
   `
 })
 export class ColorPickerComponent{
+  value: string;
   constructor() {
-
+    this.value = '#ff9000';
+  }
+  colorChange(e) {
+    this.value = document.getElementsByTagName('input')[0].value;
+    console.log(this.value);
+    
   }
 }
