8933deb09e0074403f80b7bff427682574fa028b
Quinn James <quinn@quinnjames.net>
2017-11-19 00:50:24 -0500

    Display hue variants

--- src/pages/create/create.ts
+++ src/pages/create/create.ts
@@ -11,21 +11,21 @@ export class CreatePage {
 
   currentColor: string;
   hsvColor: string[];
+  variants: string[];
   constructor(public navCtrl: NavController,
   private cl: ColorService) {
-
+    this.variants = [];
   }
   handleColorChange(event) {
     console.log(`handleColorChange(${event})`);
     this.currentColor = event;
     this.hsvColor = this.cl.hexToHsv(event);
     console.log(this.cl.hsvToHex(this.hsvColor));
-    let hueVar1 = this.cl.hsvToHex(this.cl.hueShiftHsv(this.hsvColor,90));
-    let hueVar2 = this.cl.hsvToHex(this.cl.hueShiftHsv(this.hsvColor,180));
-    let hueVar3 = this.cl.hsvToHex(this.cl.hueShiftHsv(this.hsvColor,270));
-    console.log(`${hueVar1}, ${hueVar2}, ${hueVar3}`);
-    
+    this.variants = [];
+    for (var x = 90; x < 360; x += 90) {
+      this.variants.push(this.cl.hsvToHex(this.cl.hueShiftHsv(this.hsvColor,x)));
+    console.log(this.variants);
+    }
   }
-
 
 }
